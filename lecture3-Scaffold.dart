8b742b9b9e2eedf7a99d7a4047e7ecd8d8680e86
hongoo2 <128197031+hongoo2@users.noreply.github.com>
2023-11-27 17:04:37 +0900

    Create lecture3-Scaffold.dart

---
+++ lecture3-Scaffold.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHome()
+    );
+  }
+}
+
+class MyHome extends StatelessWidget {
+  const MyHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Text('hello'),
+    );
+  }
+}
