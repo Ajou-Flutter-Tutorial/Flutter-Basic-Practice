ba9713dece1abfa0f5d87bf0ad25789890190390
hongoo2 <128197031+hongoo2@users.noreply.github.com>
2023-11-27 17:27:28 +0900

    Create lecture4-Row.dart

---
+++ lecture4-Row.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyHome());
+  }
+}
+
+class MyHome extends StatelessWidget {
+  const MyHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('hello'),
+            Text('hello'),
+            Text('hello'),
+            Text('hello'),
+          ],
+        ));
+  }
+}
